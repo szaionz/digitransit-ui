767131a7e0bc072c46fd6142c42403eb916e3899
Sammy Zaionz <56686478+szaionz@users.noreply.github.com>
2026-01-31 19:58:01 +0200

    Delete .github/workflows/scripts directory

--- .github/workflows/scripts/slack_message.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ -n "${SLACK_CHANNEL_ID}" ]; then
-    MSG='{"channel": "'$SLACK_CHANNEL_ID'","text":"'"${PUBLISHED_PACKAGES}"'\n", "username": "NPM publisher"}'
-    curl -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $SLACK_ACCESS_TOKEN" -H 'Accept: */*' -d "$MSG" 'https://slack.com/api/chat.postMessage'
-fi
